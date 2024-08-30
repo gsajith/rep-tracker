f590022f11f2d95d99e6f8d9683dedd90320a6f0
Gautham Sajith <gsajith@umich.edu>
2024-08-29 21:14:08 -0400

    First commit

---
+++ app/layout.js
@@ -0,0 +1,47 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ClerkProvider, SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "RepTracker",
+  description: "Light-weight workout tracker.",
+  generator: "Next.js",
+  manifest: "/manifest.json",
+  keywords: ["rep", "tracker", "workout"],
+  authors: [
+    {
+      name: "gsajith",
+      url: "https://gsajith.com",
+    },
+  ],
+  icons: [
+    { rel: "apple-touch-icon", url: "icons/icon-128.png" },
+    { rel: "icon", url: "icons/icon-128.png" },
+  ],
+};
+
+export const viewport = {
+  themeColor: [{ media: "(prefers-color-scheme: dark)", color: "#fff" }],
+  viewport:
+    "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover",
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <SignedOut>
+            <SignInButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
