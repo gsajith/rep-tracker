144e887a4fb1ac84dc89bee981d2243e19a4476c
Gautham Sajith <gsajith@umich.edu>
2024-08-30 13:49:19 -0400

    Add key

--- components/workout.js
+++ components/workout.js
@@ -2,7 +2,7 @@ import { parseISOString, readableDate, readableTime } from "@/utils/utils";
 import styles from "./workout.module.css"
 
 export default function Workout({ data }) {
-  return <div className={styles.container}>
+  return <div key={data.id} className={styles.container}>
     {readableDate(parseISOString(data.end_time)) + " " + readableTime(parseISOString(data.end_time))}
     <br />{data.notes}
   </div>
