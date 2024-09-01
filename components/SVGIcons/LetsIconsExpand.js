fc4b80de70f5367db17bea24c477118235219b51
Gautham Sajith <gsajith@umich.edu>
2024-09-01 19:17:56 -0400

    Current workout styling with expanding sections

---
+++ components/SVGIcons/LetsIconsExpand.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export function LetsIconsExpandDown(props) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={24}
+      height={24}
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      <path
+        fill="none"
+        stroke="currentColor"
+        strokeWidth={2}
+        d="m18 9l-6 6l-6-6"
+      ></path>
+    </svg>
+  );
+}
--- components/workout.js
+++ components/workout.js
@@ -2,9 +2,14 @@
 import { useStickyState } from '@/hooks/useStickyState';
 import styles from './workout.module.css';
 import { useEffect, useRef, useState } from 'react';
-import { calculateWorkoutTimer } from '@/utils/utils';
+import {
+  calculateDaysAgo,
+  calculateWorkoutTimer,
+  readableDate,
+} from '@/utils/utils';
 import ComboBox from './combobox';
 import ExerciseToPreview from './exerciseToPreview';
+import { LetsIconsExpandDown } from './SVGIcons/LetsIconsExpand';
 
 export default function Workout({ exerciseNames, latestExercises }) {
   const [inWorkout, setInWorkout] = useStickyState(false, 'inWorkout');
@@ -40,12 +45,26 @@ export default function Workout({ exerciseNames, latestExercises }) {
       } else {
         oldExercises.push(
           preview
-            ? preview.exercise
-            : {
-                name: name,
+            ? {
+                name: preview.exercise.name,
+                oldReps: preview.exercise.reps,
+                oldWeights: preview.exercise.weights,
+                oldNotes: preview.exercise.notes,
+                time: preview.time,
                 reps: [],
                 weights: [],
                 notes: '',
+                expanded: true,
+              }
+            : {
+                name: name,
+                oldReps: null,
+                oldWeights: null,
+                oldNotes: null,
+                reps: [],
+                weights: [],
+                notes: '',
+                expanded: true,
               }
         );
       }
@@ -79,6 +98,13 @@ export default function Workout({ exerciseNames, latestExercises }) {
     return exists === -1;
   };
 
+  const setExpanded = (index, expanded) => {
+    setExercises((oldExercises) => {
+      oldExercises[index].expanded = expanded;
+      return oldExercises;
+    });
+  };
+
   return (
     <div
       className={`${styles.container} ${!inWorkout && styles.startup}`}
@@ -93,14 +119,101 @@ export default function Workout({ exerciseNames, latestExercises }) {
       {inWorkout ? (
         <div>
           {exercises.length > 0 && (
-            <div className={styles.exerciseContainer}>
-              {exercises.map((exercise) => (
-                <div key={exercise.name} className={styles.exercise}>
-                  {exercise.name}
-                </div>
-              ))}
+            <div className={styles.exercisesContainer}>
+              {exercises.map((exercise, index) => {
+                const numOldSets =
+                  (exercise.time &&
+                    exercise.expanded &&
+                    Math.min(
+                      exercise.oldReps?.length,
+                      exercise.oldWeights?.length
+                    )) ||
+                  0;
+                return (
+                  <div
+                    key={exercise.name}
+                    className={styles.exerciseContainer}
+                    style={
+                      exercise.expanded
+                        ? { maxHeight: 150 * numOldSets + 300 }
+                        : {}
+                    }
+                  >
+                    <div
+                      className={styles.exercise}
+                      style={
+                        !(exercise.time && exercise.expanded)
+                          ? { borderRadius: 14 }
+                          : {}
+                      }
+                    >
+                      <button
+                        className={styles.exerciseName}
+                        onClick={() => {
+                          setExpanded(index, !exercise.expanded);
+                        }}
+                      >
+                        {exercise.name}{' '}
+                        <LetsIconsExpandDown
+                          style={exercise.expanded ? { rotate: '-180deg' } : {}}
+                        />
+                      </button>
+                      {exercise.expanded && (
+                        <>
+                          <span>set1</span>
+                          <span>set2</span>
+                        </>
+                      )}
+                    </div>
+                    {exercise.time && exercise.expanded && (
+                      <div className={styles.pastExercise}>
+                        <span>
+                          Previously: {readableDate(new Date(exercise.time))} (
+                          {calculateDaysAgo(new Date(exercise.time))})
+                        </span>
+                        <div className={styles.setsContainer}>
+                          {[...Array(numOldSets)].map((_e, i) => (
+                            <div className={styles.setContainer} key={i}>
+                              <span style={{ fontSize: 18 }}>
+                                {exercise.oldReps[i]}
+                              </span>
+                              <span
+                                style={{
+                                  color: '#908E96',
+                                  marginTop: 2,
+                                  fontSize: 16,
+                                }}
+                              >
+                                ×
+                              </span>
+                              <span style={{ fontSize: 18 }}>
+                                {exercise.oldWeights[i]}
+                              </span>
+                              <span
+                                style={{
+                                  color: '#908E96',
+                                  marginLeft: -3,
+                                  marginTop: 5,
+                                }}
+                              >
+                                lbs
+                              </span>
+                            </div>
+                          ))}
+                        </div>
+                        {exercise.oldNotes && exercise.oldNotes.length > 0 && (
+                          <div style={{ marginTop: 8 }}>
+                            Note: {exercise.oldNotes}
+                          </div>
+                        )}
+                      </div>
+                    )}
+                  </div>
+                );
+              })}
             </div>
           )}
+
           <div style={{ display: 'flex' }}>
             <ComboBox
               options={exerciseNames}
