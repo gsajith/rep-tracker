ecf93f80feb2a8c12804ee2472ed42f2e7676fb0
Gautham Sajith <gsajith@umich.edu>
2024-09-02 00:48:40 -0400

    Remove close icon from modal

--- components/modal.js
+++ components/modal.js
@@ -1,16 +1,10 @@
 import styles from './modal.module.css';
-import { LetsIconsTrash } from './SVGIcons/LetsIconsTrash';
 
 export default function Modal({ children, setShown }) {
   return (
     <div className={styles.modalContainer}>
       <div className={styles.shim} onClick={() => setShown(false)}></div>
-      <div className={styles.modal}>
-        <div className={styles.modalHeader}>
-          <LetsIconsTrash onClick={() => setShown(false)} />
-        </div>
-        {children}
-      </div>
+      <div className={styles.modal}>{children}</div>
     </div>
   );
 }
