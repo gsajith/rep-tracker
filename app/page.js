0faf6b2cea875e0600c053958ce9eaf3d9107b39
Gautham Sajith <gsajith@umich.edu>
2024-09-02 00:02:24 -0400

    Refactor out database fetching to util function

--- app/page.js
+++ app/page.js
@@ -3,7 +3,11 @@ import { useSession, useUser } from '@clerk/nextjs';
 import styles from './page.module.css';
 import { createClerkSupabaseClient } from '@/utils/supabase/clerk-client';
 import { useEffect, useRef, useState } from 'react';
-import { createExercise, createWorkout } from '@/utils/supabase/database';
+import {
+  createExercise,
+  createWorkout,
+  loadWorkoutWithExercises,
+} from '@/utils/supabase/database';
 import LoggedWorkout from '@/components/loggedWorkout';
 import Workout from '@/components/workout';
 import { parseISOString } from '@/utils/utils';
@@ -41,34 +45,12 @@ export default function Home() {
 
     async function loadWorkouts() {
       setLoading(true);
-      const { data, error } = await client.current.from('workouts').select();
+      const { data, error } = await loadWorkoutWithExercises(
+        client.current,
+        addExerciseName
+      );
       if (!error) {
-        for (let i = 0; i < data.length; i++) {
-          if (data[i].exercises) {
-            for (let j = 0; j < data[i].exercises.length; j++) {
-              const { data: exercise, error: exerciseError } =
-                await client.current
-                  .from('exercises')
-                  .select()
-                  .eq('id', data[i].exercises[j]);
-              if (!exerciseError) {
-                data[i].exercises[j] = exercise[0];
-                addExerciseName(
-                  exercise[0].name.toLowerCase(),
-                  data[i].end_time,
-                  exercise[0]
-                );
-              }
-            }
-          }
-        }
-        setWorkouts(
-          data.map((workout) => ({
-            ...workout,
-            start_time: parseISOString(workout.start_time),
-            end_time: parseISOString(workout.end_time),
-          }))
-        );
+        setWorkouts(data);
       }
       setLoading(false);
     }
@@ -90,37 +72,6 @@ export default function Home() {
     setExerciseNames((oldExerciseNames) => {
       return new Set([...oldExerciseNames, name]);
     });
-  }
-
-  async function handleCreateWorkout(e) {
-    e.preventDefault();
-    var d = new Date();
-    d.setDate(d.getDate() - 134);
-    var d2 = new Date();
-    d2.setDate(d2.getDate() - 134);
-    d.setHours(d.getHours() - 2);
-    d.setMinutes(d.getMinutes() - 16);
-    createWorkout(
-      client.current,
-      d.toISOString(),
-      d2.toISOString(),
-      ['7d2a19f0-c25f-43dd-9a23-27d0bb82cc88'],
-      'New function'
-    );
-  }
-
-  async function handleCreateExercise(e) {
-    e.preventDefault();
-    // createExercise(client, 'Pushups', [10, 10], [0, 0]);
-    // createExercise(client, 'Squats', [8, 8], [30, 30], 'Easy peasy');
-    // createExercise(client, 'Dumbbell row', [8], [15]);
-    // createExercise(
-    //   client,
-    //   'Laying dumbbell skullcrushers',
-    //   [14, 12, 10, 8],
-    //   [15, 15, 10, 10],
-    //   'ouch ouch ouch ouch'
-    // );
   }
 
   const saveWorkout = async (setInWorkout, workoutStartTime, exercises) => {
@@ -230,7 +181,6 @@ export default function Home() {
         </div>
       )}
 
-      {/** TODO: Do loading shimmers **/}
       {!loading &&
         workouts.length > 0 &&
         workouts
--- utils/supabase/database.js
+++ utils/supabase/database.js
@@ -1,3 +1,5 @@
+import { parseISOString } from '../utils';
+
 export async function createWorkout(
   client,
   startTime,
@@ -26,4 +28,40 @@ export async function createExercise(client, name, reps, weights, notes) {
       notes: notes,
     })
     .select();
+}
+
+export async function loadWorkoutWithExercises(client, addExerciseName) {
+  const { data, error } = await client.from('workouts').select();
+  if (!error) {
+    for (let i = 0; i < data.length; i++) {
+      if (data[i].exercises) {
+        for (let j = 0; j < data[i].exercises.length; j++) {
+          const { data: exercise, error: exerciseError } = await client
+            .from('exercises')
+            .select()
+            .eq('id', data[i].exercises[j]);
+          if (!exerciseError) {
+            data[i].exercises[j] = exercise[0];
+            addExerciseName(
+              exercise[0].name.toLowerCase(),
+              data[i].end_time,
+              exercise[0]
+            );
+          } else {
+            return { data: null, error: exerciseError };
+          }
+        }
+      }
+    }
+    return {
+      data: data.map((workout) => ({
+        ...workout,
+        start_time: parseISOString(workout.start_time),
+        end_time: parseISOString(workout.end_time),
+      })),
+      error: null,
+    };
+  } else {
+    return { data: null, error: error };
+  }
 }
