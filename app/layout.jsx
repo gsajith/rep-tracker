dd02d27a48f29db35d3c4097558fe21899ea2925
Gautham Sajith <gsajith@umich.edu>
2025-06-12 00:41:49 -0400

    Set theme-color meta

--- app/layout.jsx
+++ app/layout.jsx
@@ -27,7 +27,6 @@ export const metadata = {
 };
 
 export const viewport = {
-  themeColor: [{ media: '(prefers-color-scheme: dark)', color: '#ff0000' }],
   viewport:
     'minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover',
 };
--- context/themeProvider.jsx
+++ context/themeProvider.jsx
@@ -19,7 +19,44 @@ export const ThemeProvider = ({ children }) => {
 
   useEffect(() => {
     document.documentElement.setAttribute('data-theme', themeName);
+
+    setTimeout(() => {
+      const rootStyles = getComputedStyle(document.documentElement);
+      const themeColor = rootStyles.getPropertyValue('--background').trim();
+
+      let metaTag = document.querySelector('meta[name="theme-color"]');
+      if (!metaTag) {
+        metaTag = document.createElement('meta');
+        metaTag.setAttribute('name', 'theme-color');
+        document.head.appendChild(metaTag);
+      }
+
+      metaTag.setAttribute('content', themeColor);
+    }, 250);
   }, [themeName]);
+
+  useEffect(() => {
+    const observer = new MutationObserver(() => {
+      const rootStyles = getComputedStyle(document.documentElement);
+      const newColor = rootStyles.getPropertyValue('--background').trim();
+
+      let metaTag = document.querySelector('meta[name="theme-color"]');
+      if (!metaTag) {
+        metaTag = document.createElement('meta');
+        metaTag.setAttribute('name', 'theme-color');
+        document.head.appendChild(metaTag);
+      }
+
+      metaTag.setAttribute('content', newColor);
+    });
+
+    observer.observe(document.documentElement, {
+      attributes: true,
+      attributeFilter: ['data-theme'],
+    });
+
+    return () => observer.disconnect();
+  }, []);
 
   return (
     <ThemeContext.Provider
