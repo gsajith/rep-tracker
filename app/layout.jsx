2c21e0e33694fd3c03757d44bec3cf8e1bf06ab4
Gautham Sajith <gsajith@umich.edu>
2025-06-12 00:46:56 -0400

    Remove default from layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -27,7 +27,6 @@ export const metadata = {
 };
 
 export const viewport = {
-  themeColor: [{ media: '(prefers-color-scheme: dark)', color: '#FFFFFF' }],
   viewport:
     'minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover',
 };
