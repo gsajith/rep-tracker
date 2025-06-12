a58d30626d549038d442703da7bf7baf8f977816
Gautham Sajith <gsajith@umich.edu>
2025-06-12 17:00:07 -0400

    Rename gh stats vars

--- app/stats/page.jsx
+++ app/stats/page.jsx
@@ -2,15 +2,13 @@
 import { WorkoutsContext } from '@/context/workoutsProvider';
 import { useContext, useMemo } from 'react';
 import styles from './page.module.css';
-import Calendar from 'react-github-contribution-calendar';
+import GHCalendar from 'react-github-contribution-calendar';
 import classNames from 'classnames';
-import { parseISOString } from '@/utils/utils';
 
 export default function Stats() {
-  const { workouts, loading, loading2, exerciseNames, latestExercises } =
-    useContext(WorkoutsContext);
+  const { workouts, loading, loading2 } = useContext(WorkoutsContext);
 
-  const values = useMemo(() => {
+  const gh_values = useMemo(() => {
     const v = {};
     workouts.forEach((workout) => {
       const tzo = workout.start_time.getTimezoneOffset() * 60000;
@@ -26,14 +24,14 @@ export default function Stats() {
     return v;
   }, [workouts]);
   const tzoffset = new Date().getTimezoneOffset() * 60000;
-  const until = new Date(Date.now() - tzoffset).toISOString().split('T')[0];
+  const gh_until = new Date(Date.now() - tzoffset).toISOString().split('T')[0];
 
-  const panelColors = [
+  const gh_panelColors = [
     'rgb(from var(--accentHoverLight) r g b / 50%)',
     'rgb(from var(--accent) r g b / 70%)',
     'rgb(from var(--accent) r g b / 100%)',
   ];
-  const panelAttributes = { rx: 1, ry: 1 };
+  const gh_panelAttributes = { rx: 1, ry: 1 };
 
   return (
     <div className={styles.container}>
@@ -44,11 +42,11 @@ export default function Stats() {
           styles.calendarContainer
         )}
       >
-        <Calendar
-          values={values}
-          until={until}
-          panelColors={panelColors}
-          panelAttributes={panelAttributes}
+        <GHCalendar
+          values={gh_values}
+          until={gh_until}
+          panelColors={gh_panelColors}
+          panelAttributes={gh_panelAttributes}
         />
         <div style={{ textAlign: 'center' }}>
           {loading || loading2 ? 'Loading...' : ''}
