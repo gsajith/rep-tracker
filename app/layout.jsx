917d0dc6fa142f25658b1cbe5af1e28ad398f920
Gautham Sajith <gsajith@umich.edu>
2025-06-12 00:24:26 -0400

    Test header color

--- app/layout.jsx
+++ app/layout.jsx
@@ -27,7 +27,7 @@ export const metadata = {
 };
 
 export const viewport = {
-  themeColor: [{ media: '(prefers-color-scheme: dark)', color: '#FFFFFF' }],
+  themeColor: [{ media: '(prefers-color-scheme: dark)', color: '#ff0000' }],
   viewport:
     'minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover',
 };
