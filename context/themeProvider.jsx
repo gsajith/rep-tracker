9fb8463c14713c99b9677b7a5673fd539a6e5a0b
Gautham Sajith <gsajith@umich.edu>
2025-06-12 00:49:48 -0400

    Reset theme color a bunch more times

--- context/themeProvider.jsx
+++ context/themeProvider.jsx
@@ -17,21 +17,37 @@ export const useTheme = () => {
 export const ThemeProvider = ({ children }) => {
   const [themeName, setThemeName] = useStickyState('default', 'theme-name');
 
+  const resetThemeColor = () => {
+    const rootStyles = getComputedStyle(document.documentElement);
+    let themeColor = rootStyles.getPropertyValue('--background').trim();
+
+    if (themeColor.length < 3) {
+      themeColor = '#ECEFF3';
+    }
+
+    let metaTag = document.querySelector('meta[name="theme-color"]');
+    if (!metaTag) {
+      metaTag = document.createElement('meta');
+      metaTag.setAttribute('name', 'theme-color');
+      document.head.appendChild(metaTag);
+    }
+
+    metaTag.setAttribute('content', themeColor);
+  };
+
+  useEffect(() => {
+    resetThemeColor();
+  }, []);
+
   useEffect(() => {
     document.documentElement.setAttribute('data-theme', themeName);
 
+    resetThemeColor();
     setTimeout(() => {
-      const rootStyles = getComputedStyle(document.documentElement);
-      const themeColor = rootStyles.getPropertyValue('--background').trim();
-
-      let metaTag = document.querySelector('meta[name="theme-color"]');
-      if (!metaTag) {
-        metaTag = document.createElement('meta');
-        metaTag.setAttribute('name', 'theme-color');
-        document.head.appendChild(metaTag);
-      }
-
-      metaTag.setAttribute('content', themeColor);
+      resetThemeColor();
+    }, 100);
+    setTimeout(() => {
+      resetThemeColor();
     }, 250);
   }, [themeName]);
 
