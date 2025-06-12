42fcb627d8a00475d09179f4914acdaf49688a96
Gautham Sajith <gsajith@umich.edu>
2025-06-12 00:43:33 -0400

    Add back default themeColor

--- app/layout.jsx
+++ app/layout.jsx
@@ -27,6 +27,7 @@ export const metadata = {
 };
 
 export const viewport = {
+  themeColor: [{ media: '(prefers-color-scheme: dark)', color: '#FFFFFF' }],
   viewport:
     'minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover',
 };
