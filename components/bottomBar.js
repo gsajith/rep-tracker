a39cb459df234b8f2c31e177857209c961e9ea48
Gautham Sajith <gsajith@umich.edu>
2025-06-12 15:25:00 -0400

    Bottom bar active status

--- components/bottomBar.js
+++ components/bottomBar.js
@@ -1,4 +1,5 @@
-import Link from 'next/link';
+'use client';
+import { usePathname } from 'next/navigation';
 import styles from './bottomBar.module.css';
 import BottomBarButton from './bottomBarButton';
 import { LetsIconsHome } from './SVGIcons/LetsIconsHome';
@@ -6,15 +7,18 @@ import { LetsIconsSettings } from './SVGIcons/LetsIconsSettings';
 import { LetsIconsStats } from './SVGIcons/LetsIconsStats';
 
 export default function BottomBar() {
+  const pathname = usePathname();
+
+  console.log(pathname);
   return (
     <div className={styles.bottomBarContainer}>
-      <BottomBarButton active href={'/'}>
+      <BottomBarButton active={pathname === '/'} href={'/'}>
         <LetsIconsHome /> Home
       </BottomBarButton>
-      <BottomBarButton active={false} href={'/stats'}>
+      <BottomBarButton active={pathname === '/stats'} href={'/stats'}>
         <LetsIconsStats /> Stats
       </BottomBarButton>
-      <BottomBarButton active={false} href={'/settings'}>
+      <BottomBarButton active={pathname === '/settings'} href={'/settings'}>
         <LetsIconsSettings /> Settings
       </BottomBarButton>
     </div>
